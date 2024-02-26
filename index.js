686f207218a609bf88d8a113224d046032babd56
Konstantinos Arvanitakis <ksarvanitakis@hotmail.com>
2024-02-26 10:35:06 +0100

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 
+const para = document.createElement("p")
+console.log(para);
+
+const altPara = para.innerHTML = "I want to do skydiving";
+
+const list = document.querySelector('.list');
+list.append(altPara)
+console.log(list) 
+
